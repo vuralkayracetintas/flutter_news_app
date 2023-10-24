ea827260043b90d37a343c6b597707402fd76f0a
vuralkayracetintas <vuralkayracetintass@gmail.com>
2023-10-24 14:55:09 +0300

    initial commit

---
+++ lib/core/home/home_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [const Text('text')],
+        ),
+      ),
+    );
+  }
+}
