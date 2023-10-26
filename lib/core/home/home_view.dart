b9ee32718329328c64020f4eee45b46a2987766c
vuralkayracetintas <vuralkayracetintass@gmail.com>
2023-10-26 15:06:56 +0300

    category

--- lib/core/home/home_view.dart
+++ lib/core/home/home_view.dart
@@ -43,6 +43,7 @@ class _HomeViewState extends State<HomeView> {
                 },
                 child: Column(
                   children: [
+                    TextField(),
                     NewsCategoryList(),
                     Expanded(
                       child: CountryHomeView(
